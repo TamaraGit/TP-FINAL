d332779ad08773713a4e1d03b5cf9221235c5a64
Tama <tamararfrias@gmail.com>
2023-04-19 11:21:27 -0300

    Mi ultimo fracaso en JS

--- js/script.js
+++ js/script.js
@@ -14,3 +14,47 @@ fetch("./Database/database.json")
             `
         }
 })
+
+/* 
+const formularioUsuario = document.querySelector("#formularioUsuario")
+
+const lugares = []
+/* const renderizarLugares = () => */
+/*
+formularioUsuario.addEventListener("submit", (event) =>{
+    event.preventDefault()
+    console.log(formularioUsuario.lugares.value)
+    lugares.push({formularioUsuario.nochecitas.value})
+})
+ */
+
+
+
+const formularioCotizador = document.querySelector("#formularioCotizador")
+const contenedorHTML2 = document.querySelector(".contenedorCotizador") 
+const sitios = []
+
+ const renderizarSitios = () =>{
+    contenedorHTML2.innerHTML = ""
+    for(const sitio of sitios){
+        contenedorHTML2.innerHTML += `
+        <div class="card-sitios">
+            <h2>Nombre del lugar: ${sitios.lugares}</h2>
+            <p>Cant de noches: ${sitios.noches}</p>
+
+        </div>
+        `
+    }
+} 
+
+formularioCotizador.addEventListener("submit", (event) =>{
+    event.preventDefault()
+    console.log(formularioCotizador.lugares.value)
+    console.log(formularioCotizador.noches.value)
+    sitios.push({
+    /* formularioCotizador.lugares.value */
+    formularioCotizador.noches.value
+   
+    })
+    renderizarSitios() 
+})
